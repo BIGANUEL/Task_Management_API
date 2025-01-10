3fee478861cc64424754c8670077b317efbdd7e9
Amanuel Wubishet <162797908+BIGANUEL@users.noreply.github.com>
2025-01-11 00:06:24 +0300

    Update settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -190,5 +190,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ashenafimulugeta016@gmail.com'
+EMAIL_HOST_USER = 'biganuel6792@gmail.com'
 EMAIL_HOST_PASSWORD = 'sfyuexavldrmpiqo'  # or your regular password if 2FA is not enabled
